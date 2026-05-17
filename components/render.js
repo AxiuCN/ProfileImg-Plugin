eddfe76fcdefb8e952692da4a2cf93a5c79882a2
阿修Axiu <1194310418@qq.com>
2026-05-18 03:56:05 +0800

    Update render.js

--- components/render.js
+++ components/render.js
@@ -6,20 +6,25 @@ import { currentVersion, yunzaiVersion } from './pluginVersion.js'
 const pluginRoot = path.join(process.cwd(), 'plugins/ProfileImg-Plugin')
 
 export async function render(app, tpl, data = {}, imgType = 'png') {
-  const dataDir = path.join(process.cwd(), 'data', 'html', 'ProfileImg-Plugin', app, tpl)
-  fs.mkdirSync(dataDir, { recursive: true })
-
   data._plugin = 'ProfileImg-Plugin'
   data._res_path = `../../../../../plugins/ProfileImg-Plugin/resources/`
-  data.pluResPath = data._res_path   // 框架内部依赖 pluResPath
+  data.pluResPath = data._res_path
   data.tplFile = path.join(pluginRoot, 'resources', app, `${tpl}.html`)
   data.saveId = data.saveId || data.save_id || tpl
   data.imgType = imgType
-  if (imgType === 'png') data.omitBackground = true
-  data.pageGotoParams = { waitUntil: 'networkidle0' }
+
+  if (imgType === 'png') {
+    data.omitBackground = true
+  }
+  data.pageGotoParams = {
+    waitUntil: 'networkidle0'
+  }
   data.sys = {
     copyright: `Created By Yunzai-Bot<span class="version">${yunzaiVersion}</span> & ProfileImg-Plugin<span class="version">${currentVersion}</span>`
   }
+
+  const dataDir = path.join(process.cwd(), 'data', 'html', 'ProfileImg-Plugin', app, tpl)
+  fs.mkdirSync(dataDir, { recursive: true })
 
   return await puppeteer.screenshot(`ProfileImg-Plugin/${app}/${tpl}`, data)
 }
