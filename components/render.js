2d1dc042a51bcf84779bac41f315cefcf604dd46
阿修Axiu <1194310418@qq.com>
2026-05-18 21:28:35 +0800

    Update render.js

--- components/render.js
+++ components/render.js
@@ -3,35 +3,34 @@ import path from 'node:path'
 import fs from 'node:fs'
 import { currentVersion, yunzaiVersion } from './pluginVersion.js'
 
-const plugin = 'ProfileImg-Plugin'
-const _path = process.cwd()
+const pluginRoot = path.join(process.cwd(), 'plugins/ProfileImg-Plugin')
 
-data.elemLayout = path.join(pluginRoot, 'resources', 'common', 'layout', 'elem.html')
-data.defaultLayout = path.join(pluginRoot, 'resources', 'common', 'layout', 'default.html')
+export async function render(app, tpl, data = {}, imgType = 'jpeg') {
+  data._plugin = 'ProfileImg-Plugin'
+  data._res_path = `../../../../../plugins/ProfileImg-Plugin/resources/`
 
-export async function render(app = '', tpl = '', data = {}, imgType = 'jpeg') {
-  data._plugin = plugin
-  data._res_path = `../../../../../plugins/${plugin}/resources/`
-  if (imgType == "png") {
+  if (imgType === 'png') {
     data.omitBackground = true
   }
   data.imgType = imgType
 
-  // 对齐 Data.createDir 的行为
-  const dataDir = path.join(_path, 'data', 'html', plugin, app, tpl)
+  // 创建缓存目录
+  const dataDir = path.join(process.cwd(), 'data', 'html', 'ProfileImg-Plugin', app, tpl)
   fs.mkdirSync(dataDir, { recursive: true })
 
   data.saveId = data.saveId || data.save_id || tpl
-  data.tplFile = `./plugins/${plugin}/resources/${app}/${tpl}.html`
+  data.tplFile = `./plugins/ProfileImg-Plugin/resources/${app}/${tpl}.html`
   data.pluResPath = data._res_path
-  data.pageGotoParams = {
-    waitUntil: 'networkidle0'
-  }
+  data.pageGotoParams = { waitUntil: 'networkidle0' }
+
+  // 注入布局文件路径
+  data.elemLayout = path.join(pluginRoot, 'resources', 'common', 'layout', 'elem.html')
+  data.defaultLayout = path.join(pluginRoot, 'resources', 'common', 'layout', 'default.html')
 
   // 注入系统版权信息
   data.sys = {
     copyright: `Created By Yunzai-Bot<span class="version">${yunzaiVersion}</span> & ProfileImg-Plugin<span class="version">${currentVersion}</span>`
   }
 
-  return await puppeteer.screenshot(`${plugin}/${app}/${tpl}`, data)
+  return await puppeteer.screenshot(`ProfileImg-Plugin/${app}/${tpl}`, data)
 }
